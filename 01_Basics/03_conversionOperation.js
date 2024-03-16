9b7428781fc83b80fab667049d7b9b8089b178e7
Rishiraj Aman <78068099+hellzom@users.noreply.github.com>
2024-03-16 10:23:51 +0000

    Conversion of datatypes (typecasting)

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+let cValue = String(score)
+
+console.log(typeof(cValue))
+console.log(cValue);
+
+
+/*
+TypeCasting - 
+1. Number()
+"33" => 33
+"3abc" => NaN
+"true/false" => 1/0
+null => 0
+undefined => NaN
+
+2. String()
+"33" => "33"
+33 => "33"
+null => null
+undefined => undefined
+
+3. Boolean()
+1 => true
+0 => false
+"" => false
+"sfgsd" => true
+*/
