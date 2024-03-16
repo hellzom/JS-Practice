d143347ca238bde197134bdbdab83f5e1c82346b
Rishiraj Aman <78068099+hellzom@users.noreply.github.com>
2024-03-16 10:40:08 +0000

    Operations

---
+++ 01_Basics/03_1_operations.js
@@ -0,0 +1,35 @@
+let value = 3
+let negValue = -3
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " rishi"
+console.log(str1+str2);
+
+console.log("1" +2); //12
+console.log(1 + "2"); // 12
+console.log("1" + "2"); //12
+console.log(1 + 2 + "2"); //32
+console.log("2" + 1 + 3); //213
+console.log(1 + 2 + "2" + 1 + 3); //3213
+
+
+// If string is at first, everything will be treated as string.
+// If string is at last, previous values will be calculated and then concated with the string value.
+// Where ever the string is placed, values after that will be concated, not calculated.
+
+
+console.log(+true); // 1
+// console.log(true+); // error
+console.log(+""); // 0
+
+let num1, num2, num3;
+
+
